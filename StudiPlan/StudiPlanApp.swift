eab2d1e180ac7295b461e9e100ed95fbab0fc90e
Raaina <raaina.daniar@gmail.com>
2023-10-12 19:20:25 -0400

    Initial Commit

---
+++ StudiPlan/StudiPlanApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudiPlanApp.swift
+//  StudiPlan
+//
+//  Created by Mohamad Daniar on 10/12/23.
+//
+
+import SwiftUI
+
+@main
+struct StudiPlanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
