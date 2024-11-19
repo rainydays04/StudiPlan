94f1d4b26f713019ddc2d4cf3b669f9dd053c6ed
Raaina <raaina.n.daniar@gmail.com>
2024-11-18 20:58:42 -0500

    started working on the journaling page

---
+++ StudiPlan/Journaling.swift
@@ -0,0 +1,18 @@
+//
+//  Journaling.swift
+//  StudiPlan
+//
+//  Created by Mohamad Daniar on 11/18/24.
+//
+
+import SwiftUI
+
+struct Journaling: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Journaling()
+}
---
+++ StudiPlan/NoteRow.swift
@@ -0,0 +1,18 @@
+//
+//  NoteRow.swift
+//  StudiPlan
+//
+//  Created by Mohamad Daniar on 11/18/24.
+//
+
+import SwiftUI
+
+struct NoteRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NoteRow()
+}
