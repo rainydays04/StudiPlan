67dbd05746f285eddf87814e4da19cf51c2bf946
Raaina <raaina.n.daniar@gmail.com>
2024-11-21 14:00:50 -0500

    added notes stuff to add to the page

---
+++ StudiPlan/NoteMaster.swift
@@ -0,0 +1,18 @@
+//
+//  NoteMaster.swift
+//  StudiPlan
+//
+//  Created by Mohamad Daniar on 11/21/24.
+//
+
+import SwiftUI
+
+struct NoteMaster: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NoteMaster()
+}
